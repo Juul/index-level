9fbff55275aa4a992626c77c1ea202ed418ad3b3
Marc Juul <github@juul.io>
2016-11-14 17:47:45 -0800

    added support for simple indexes (property name/path) and a few API/docs improvements

--- README.md
+++ README.md
@@ -34,7 +34,20 @@ index.add('foo', {async: true}, function(key, value, cb) {
 
 # API
 
-## .add(indexName, opts, indexFunction) 
+## .indexer(db, idb, [opts])
+
+The constructor.
+
+* db: The leveldb or sublevel containing your data.
+* idb: The leveldb or sublevel to use for index storage.
+
+Opts:
+
+```
+rebuild: false // force rebuild on init
+```
+
+## .add(indexName, [opts], indexFunction) 
 
 Add a new index. This function is synchronous.
 
@@ -46,6 +59,14 @@ Opts:
 ```
  async: false // set to true if indexFunc uses a callback
 ```
+
+The indexFunction will receive the arguments like so:
+
+```
+indexFunction(key, value, cb); // cb only present if .add called with {async: true}
+```
+
+For {async: false} the indexFunction must return the index key. For {async: true} the callback must be called with the index key as the second argument and potential errors or null/undefined as the first argument.
 
 If no index should be created for the current db key/value pair then the index key returned by indexFunction must be either `undefined` or `null`.
 
@@ -61,8 +82,6 @@ Add a new index. This function is synchronous.
 indexProperty can be a path, e.g. `creator.name`.
 
 ## .add(indexProperty) 
-
-TODO not implemented
 
 Add a new index. This function is synchronous.
 
@@ -123,11 +142,8 @@ Call .rebuild for all indexes.
 
 # TODO
 
-* Add .createValueStream and .createKeyStream
-* Easy simple indexes (index by object property or sub-property)
 * Unit tests
-* Option to return a leveldown
-
+* Add option to return a leveldown
 
 # License and copyright
 
--- index.js
+++ index.js
@@ -5,6 +5,25 @@ var xtend = require('xtend');
 var async = require('async');
 var changes = require('level-changes');
 
+
+// resolve a path like ['foo', 'bar', 'baz']
+// to return the value of obj.foo.bar.baz
+// or undefined if tha path does not exist
+function resolvePropPath(obj, path) {
+
+  if(path.length > 1) {
+    if(!obj[path[0]]) return undefined;
+
+    return resolvePropPath(obj[path[0]], path.slice(1, path.length));
+  }
+
+  if(path.length === 1) {
+    return obj[path[0]];
+  }
+
+  return undefined;
+}
+
 function indexer(db, idb, opts) {
   if(!(this instanceof indexer)) return new indexer(db, idb, opts);
 
@@ -13,7 +32,7 @@ function indexer(db, idb, opts) {
   this.rdb = sublevel(idb, 'r'); // the reverse lookup db
 
   this.opts = xtend({
-
+    rebuild: false // force rebuild on init
   }, opts || {});
 
   this.indexes = {};
@@ -26,6 +45,10 @@ function indexer(db, idb, opts) {
       this._deleteFromIndex(change.key);
     }
   }.bind(this));
+
+  if(this.opts.rebuild) {
+    this.rebuildAll();
+  }
 
   this._updateIndexes = function(key, value, cb) {
     cb = cb || this._nullFunc;
@@ -55,7 +78,11 @@ function indexer(db, idb, opts) {
           }], cb);
         })
       } else {
-        var indexKey = idx.f(key, value);
+        try {
+          var indexKey = idx.f(key, value);
+        } catch(err) {
+          return cb(err);
+        }
 
         if(indexKey === undefined || indexKey === null) return cb();
         async.parallel([function(cb) {
@@ -83,15 +110,46 @@ function indexer(db, idb, opts) {
     }
   }
 
-  this.add = function(name, opts, indexFunc) {
-    if(typeof opts === 'function') {
-      indexFunc = opts;
+  // return an indexer that indexes by property
+  this._propIndexer = function(propPath, opts) {
+    opts = opts || {};
+    if(!(propPath instanceof Array)) propPath = propPath.split('.');
+
+    return function(key, value) {
+      return resolvePropPath(value, propPath);
+    };
+  }
+
+  this.add = function(name, opts, indexBy) {
+
+    // check for calling convention: .add(['propName1', 'propName2'])
+    if(name instanceof Array) {
+      var i;
+      for(i=0; i < name.length; i++) {
+        this.add(name[i], opts);
+      }
+      return;
+    }
+
+    // check for calling without opts but with index function or property name
+    if(typeof opts === 'function' || typeof opts === 'string') {
+      indexBy = opts;
       opts = {};
     }
 
     opts = xtend({
       async: false // set to true if indexFunc uses a callback
     }, opts || {});
+
+    var indexFunc;
+    // check for calling convention: .add('propName', [opts])
+    if(!indexBy && (!opts || typeof opts === 'object')) {
+      indexFunc = this._propIndexer(name);
+
+    // check for calling convention: .add('indexName', [opts], 'propName')
+    } else if(typeof indexBy === 'string') {
+      indexFunc = this._propIndexer(indexBy);
+    }
 
     if(this.indexes[name]) return new Error("Index already exists");
     this.indexes[name] = {
@@ -243,20 +301,43 @@ function indexer(db, idb, opts) {
   };
 
   this.createReadStream = function(indexName, opts) {
+    opts = xtend({
+      keys: true, // output keys
+      values: true // output values
+    }, opts || {});    
     var idx = this.indexes[indexName];
     if(!idx) return cb(new Error("Index does not exist"));
 
     var out = through.obj(function(obj, enc, next) {
       idx.db.get(obj.key, function(err, value) {
         if(err) return next(err);
-        this.push({key: obj.key, value: value});
+        if(opts.keys && opts.valyues) {
+          this.push({key: obj.key, value: value});
+        } else if(opts.values) {
+          this.push(value);
+        } else {
+          this.push(obj.key);
+        }
         next();
       }.bind(this));
     });
     
     idx.db.createReadStream(opts).pipe(out);
     return out;
+  };
 
+  this.createKeyStream = function(indexName, opts) {
+    opts = opts || {};
+    opts.keys = true;
+    opts.values = false;
+    return this.createReadStream(indexName, opts);
+  };
+
+  this.createValueStream = function(indexName, opts) {
+    opts = opts || {};
+    opts.keys = false;
+    opts.values = true;
+    return this.createReadStream(indexName, opts);
   };
 
 }
